da63ed0c429d358de6cb3499712b2c56386fda98
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-04-04 11:14:42 +0000

    Added the Destination Page 2.0

--- src/components/Destination/Main.js
+++ src/components/Destination/Main.js
@@ -9,11 +9,11 @@ import titan from '../../assets/destination/image-titan.png'
 function Main(props) {
 
   const [jsondata, setJsonData] = useState(data)
-    const [planetid, setPlanetId] = useState(0)
+    const [planetid, setPlanetId] = useState(1)
 
     let planetImages = [moon, mars, europa, titan]
   return (
-    <div className='flex flex-col justify-around items-center pt-5 pb-14'>
+    <div className='flex flex-col justify-around items-center pt-5 pb-10'>
       <h2 className='text-white text-center uppercase font-barlow tracking-widest text-lg mt-4'><span className='brightness-50 mr-2'>01</span> Pick your destination </h2>
       <div className='text-white flex flex-col items-center justify-center'>
         <img src={planetImages[planetid]} alt='' className='w-[250px] p-4' />
