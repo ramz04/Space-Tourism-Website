8e553ba7ee403adfa40fbb54c039e5a4ca53e481
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-04-29 11:51:30 +0000

    Update Main.js

--- src/components/Technology/Main.js
+++ src/components/Technology/Main.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Main() {
   return (
-    <div>Main</div>
+    <div className='flex flex-col justify-around text-white items-center pt-5 pb-10'>
+        <h2 className='text-white text-center uppercase font-barlow tracking-widest text-lg mt-4'><span className='brightness-50 mr-2'>03</span> Space launch 101 </h2>
+    </div>
   )
 }
 
