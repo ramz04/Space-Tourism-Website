75a2358cad95db4bbd95137db8cefa9c025e128d
Abdul-Rahman Wunpini <55752850+ramz04@users.noreply.github.com>
2023-04-29 17:07:23 +0000

    Added Destination Desktop Page

--- src/components/Destination/Main.js
+++ src/components/Destination/Main.js
@@ -24,27 +24,29 @@ function Main(props) {
 
     let planetImages = [moon, mars, europa, titan]
   return (
-    <div className='pt-5 pb-10'>
-      <h2 className='text-white text-center sm:pl-6 sm:text-left uppercase font-barlow tracking-widest text-lg mt-4'><span className='brightness-50 mr-2'>01</span> Pick your destination </h2>
-      <div className='text-white flex flex-col sm:mt-6 items-center justify-center'>
-        <img src={planetImages[planetid]} alt='' className='w-[250px] p-4' />
-        <div ref={navigation} className='destination--content--nav flex justify-center gap-4 sm:gap-10 sm:mt-4 uppercase'>
-          <div className='cursor-pointer active' data-index={0} onClick={tabChange} >moon</div>
-          <div className='cursor-pointer' data-index={1} onClick={tabChange}>mars</div>
-          <div className='cursor-pointer' data-index={2} onClick={tabChange}>europa</div>
-          <div className='cursor-pointer' data-index={3} onClick={tabChange}>titan</div>
-        </div>
-        <h1 className='text-center text-7xl font-bellefair mt-4 sm:mt-6 uppercase'>{jsondata.destinations[planetid].name}</h1>
-        <p className='p-6 tracking-wider text-[#D0D6F9] brightness-75 text-center sm:px-14'>{jsondata.destinations[planetid].description}</p>
-        <hr className='bg-[#D0D6F9] mt-  sm:mt-10 w-[80%] brightness-50' />
-        <div className='sm:flex mt-6 sm:items-center sm:gap-20 mb-4'>
-          <div className='text-center'>
-            <p className=''>AVG. DISTANCE</p>
-            <h2 className='text-4xl pt-2 font-bellefair uppercase'>{jsondata.destinations[planetid].distance}</h2>
+    <div className='pt-5 pb-10 lg:pb-0 lg:pt-0'>
+      <h2 className='text-white text-center sm:pl-6 lg:pl-48 lg:text-2xl lg:mt-6 sm:text-left uppercase font-barlow tracking-widest text-lg mt-4 lg:mt-8'><span className='brightness-50 mr-2'>01</span> Pick your destination </h2>
+      <div className='lg:flex lg:px-4 lg:justify-center'>
+        <img src={planetImages[planetid]} alt='' className='w-[250px] p-4 lg:p-0 lg:scale-[60%] lg:basis-1/2' />
+        <div className='text-white flex flex-col sm:mt-6 lg:mt-0 items-center lg:items-start justify-center lg:basis-1/2'>
+          <div ref={navigation} className='destination--content--nav flex justify-center lg:text-lg lg:justify-start gap-4 sm:gap-10 sm:mt-4 uppercase'>
+            <div className='cursor-pointer active' data-index={0} onClick={tabChange} >moon</div>
+            <div className='cursor-pointer' data-index={1} onClick={tabChange}>mars</div>
+            <div className='cursor-pointer' data-index={2} onClick={tabChange}>europa</div>
+            <div className='cursor-pointer' data-index={3} onClick={tabChange}>titan</div>
           </div>
-          <div className='text-center mt-4 sm:mt-0'>
-            <p>EST. TRAVEL TIME</p>
-            <h2 className='text-4xl font-bellefair pt-2 uppercase'>{jsondata.destinations[planetid].travel}</h2>
+          <h1 className='text-center lg:text-left text-7xl lg:text-[120px] font-bellefair mt-4 sm:mt-6 uppercase'>{jsondata.destinations[planetid].name}</h1>
+          <p className='p-6 tracking-wider lg:text-left text-[#D0D6F9] brightness-75 lg:px-0 lg:pr-80 text-center sm:px-14'>{jsondata.destinations[planetid].description}</p>
+          <hr className='bg-[#D0D6F9] lg:mt-6  sm:mt-10 w-[80%] brightness-50' />
+          <div className='sm:flex mt-6 sm:items-center sm:gap-20 lg:gap-24 mb-4'>
+            <div className='text-center lg:text-left'>
+              <p className=''>AVG. DISTANCE</p>
+              <h2 className='text-4xl pt-2 font-bellefair uppercase'>{jsondata.destinations[planetid].distance}</h2>
+            </div>
+            <div className='text-center lg:text-left mt-4 sm:mt-0'>
+              <p>EST. TRAVEL TIME</p>
+              <h2 className='text-4xl font-bellefair pt-2 uppercase'>{jsondata.destinations[planetid].travel}</h2>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -5,18 +5,18 @@ function Main() {
   return (
     <div className='text-white px-6 sm:px-24 sm:mt-10 lg:px-0 py-8 flex flex-col lg:flex-row sm:justify-around justify-between gap-20 h-[80%] items-center'>
         <div className='text-center lg:text-left lg:basis-1/2'>
-            <h3 className='uppercase tracking-widest sm:text-lg text-[#D0D6F9] lg:text-xl font-light font-barlow'>So, you want to travel to</h3>
+            <h3 className='uppercase tracking-widest sm:text-lg lg:text-3xl text-[#D0D6F9] font-light font-barlow'>So, you want to travel to</h3>
             <h1 className='mt-4 uppercase text-7xl sm:text-[120px] lg:text-9xl lg:tracking-widest sm:tracking-widest tracking-wide '>
                 Space
             </h1> 
-            <p className='mt-6 text-[#D0D6F9]  tracking-wider sm:px-10 font-barlow'>
-            Let’s face it; if you want to go to space, you might as well genuinely go to 
-  outer space and not hover kind of on the edge of it. Well sit back, and relax 
-  because we’ll give you a truly out of this world experience!
+            <p className='mt-6 text-[#D0D6F9]  tracking-wider lg:px-0 lg:pr-80 lg:text-[19px] sm:px-10 font-barlow'>
+              Let’s face it; if you want to go to space, you might as well genuinely go to 
+              outer space and not hover kind of on the edge of it. Well sit back, and relax 
+              because we’ll give you a truly out of this world experience!
             </p>
         </div>
         <Link to={`destination`}>
-          <div className='uppercase mt-16 text-4xl py-[6.8rem] lg:py-32 lg:px-20  lg:-mt-0 px-[3.2rem] rounded-full bg-white text-[#0B0D17]'>
+          <div className='uppercase mt-16 text-4xl lg:-mb-36 py-[6.8rem] lg:py-32 lg:px-[4.5rem]  lg:-mt-0 px-[3.2rem] rounded-full bg-white text-[#0B0D17]'>
               Explore
           </div>
         </Link>
